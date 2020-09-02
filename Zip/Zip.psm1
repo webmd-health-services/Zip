65abe967be362a51612f1997b3728c275e58a3cf
Joseph Larionov <jlarionov@webmd.net>
2020-09-02 16:17:44 -0700

    Fix failing test on PowerShell Core and Linux

--- Zip/Zip.psm1
+++ Zip/Zip.psm1
@@ -1,3 +1,10 @@
+# PowerShell 5.1 doesn't have these variables so create them if they don't exist.
+if( -not (Get-Variable -Name 'IsLinux' -ErrorAction Ignore) )
+{
+    $IsLinux = $false
+    $IsMacOS = $false
+    $IsWindows = $true
+}
 
 Add-Type -AssemblyName 'System.IO.Compression'
 Add-Type -AssemblyName 'System.IO.Compression.FileSystem'
