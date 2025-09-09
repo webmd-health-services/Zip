cc9c1a568ab232e84b39da4e6f6bc35f254de03b
Aaron Jensen <ajensen@webmd.net>
2025-09-09 16:37:18 -0700

    Migrating tests to Pester 5.

--- Tests/New-ZipArchive.Tests.ps1
+++ Tests/New-ZipArchive.Tests.ps1
@@ -2,171 +2,164 @@
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\Zip' -Resolve) -Force
-
-$result = $null
-
-function GivenFile
-{
-    param(
-        [string[]]
-        $Path
-    )
-
-    foreach( $pathItem in $Path )
+BeforeDiscovery {
+    if (-not (Test-Path -Path 'variable:IsWindows'))
     {
-        $fullPath = Join-Path -Path $TestDrive.FullName -ChildPath $pathItem
-
-        $parentDir = $fullPath | Split-Path
-        if( -not (Test-Path -Path $parentDir -PathType Container) )
-        {
-            New-Item -Path $parentDir -ItemType 'Directory'
-        }
-
-        if( -not (Test-Path -Path $fullPath -PathType Leaf) )
-        {
-            New-Item -Path $fullPath -ItemType 'File'
-        }
+        $script:IsWindows = $true
+        $script:IsLinux = $false
+        $script:IsMacOS = $false
     }
 }
 
-function Init
-{
+BeforeAll {
+    Set-StrictMode -Version 'Latest'
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\Zip' -Resolve) -Force
+
     $script:result = $null
-}
+    $script:testDirPath = $null
+    $script:testNum = 0
 
-function ThenArchiveCreated
-{
-    param(
-        $ExpectedPath
-    )
-
-    Test-Path -LiteralPath $result.FullName -PathType Leaf | Should -BeTrue
-    $result | Should -BeOfType ([IO.FileInfo])
-    $result.FullName | Should -Be (Join-Path -Path $TestDrive.FullName -ChildPath $ExpectedPath)
-
-    $zipExpandPath = Join-Path -Path $TestDrive.FullName -ChildPath ('zip.{0}' -f [IO.Path]::GetRandomFileName())
-    { Expand-Archive -LiteralPath $result.FullName -DestinationPath $zipExpandPath } | Should -Not -Throw
-}
-
-function ThenError
-{
-    param(
-        $Matches
-    )
-
-    $Global:Error | Should -Match $Matches
-}
-
-function ThenNothingReturned
-{
-    $result | Should -BeNullOrEmpty
-}
-
-function WhenCreatingArchive
-{
-    [CmdletBinding()]
-    param(
-        $Name,
-        [Switch]
-        $WithRelativePath,
-        [Switch]
-        $Force
-    )
-
-    $path = Join-Path -Path $TestDrive.FullName -ChildPath $Name
-    if( $WithRelativePath )
+    function GivenFile
     {
-        Push-Location $TestDrive.FullName
-        $path = $Name
+        param(
+            [string[]]
+            $Path
+        )
+
+        foreach( $pathItem in $Path )
+        {
+            $fullPath = Join-Path -Path $script:testDirPath -ChildPath $pathItem
+
+            $parentDir = $fullPath | Split-Path
+            if( -not (Test-Path -Path $parentDir -PathType Container) )
+            {
+                New-Item -Path $parentDir -ItemType 'Directory'
+            }
+
+            if( -not (Test-Path -Path $fullPath -PathType Leaf) )
+            {
+                New-Item -Path $fullPath -ItemType 'File'
+            }
+        }
     }
 
-    try
+    function ThenArchiveCreated
     {
-        $Global:Error.Clear()
-        $script:result = New-ZipArchive -Path $path -Force:$Force
+        param(
+            $ExpectedPath
+        )
+
+        Test-Path -LiteralPath $script:result.FullName -PathType Leaf | Should -BeTrue
+        $script:result | Should -BeOfType ([IO.FileInfo])
+        $script:result.FullName | Should -Be (Join-Path -Path $script:testDirPath -ChildPath $ExpectedPath)
+
+        $zipExpandPath = Join-Path -Path $script:testDirPath -ChildPath ('zip.{0}' -f [IO.Path]::GetRandomFileName())
+        { Expand-Archive -LiteralPath $script:result.FullName -DestinationPath $zipExpandPath } | Should -Not -Throw
     }
-    catch
+
+    function ThenError
     {
-        Write-Error -ErrorRecord $_
+        param(
+            $MatchesRegex
+        )
+
+        $Global:Error | Should -Match $MatchesRegex
     }
-    finally
+
+    function ThenNothingReturned
     {
+        $script:result | Should -BeNullOrEmpty
+    }
+
+    function WhenCreatingArchive
+    {
+        [CmdletBinding()]
+        param(
+            $Name,
+            [Switch]
+            $WithRelativePath,
+            [Switch]
+            $Force
+        )
+
+        $path = Join-Path -Path $script:testDirPath -ChildPath $Name
         if( $WithRelativePath )
         {
-            Pop-Location
+            Push-Location $script:testDirPath
+            $path = $Name
+        }
+
+        try
+        {
+            $Global:Error.Clear()
+            $script:result = New-ZipArchive -Path $path -Force:$Force
+        }
+        catch
+        {
+            Write-Error -ErrorRecord $_
+        }
+        finally
+        {
+            if( $WithRelativePath )
+            {
+                Pop-Location
+            }
         }
     }
 }
 
-Describe 'New-ZipArchive.when passing absolute path' {
-    It 'should create archive at that path' {
-        Init
+Describe 'New-ZipArchive' {
+    BeforeEach {
+        $script:result = $null
+        $script:testDirPath = Join-Path -Path $TestDrive -ChildPath ($script:testNum++)
+        New-Item -Path $script:testDirPath -ItemType Directory
+    }
+
+    It 'supports absolute paths' {
         WhenCreatingArchive 'somefile.zip'
         ThenArchiveCreated 'somefile.zip'
     }
-}
 
-Describe 'New-ZipArchive.when passing relative path' {
-    It 'should resolve path relative to working directory' {
-        Init
+    It 'supports relative paths' {
         WhenCreatingArchive 'somefile.zip' -WithRelativePath
         ThenArchiveCreated 'somefile.zip'
     }
-}
 
-Describe 'New-ZipArchive.when file exists' {
-    It 'should fail' {
-        Init
+    It 'preserves existing file' {
         GivenFile 'somefile.zip'
         WhenCreatingArchive 'somefile.zip' -ErrorAction SilentlyContinue
         ThenNothingReturned
         ThenError -Matches 'already exists'
     }
-}
 
-Describe 'New-ZipArchive.when file exists and forcing creation' {
-    It 'should clobber original file' {
-        Init
+    It 'clobbers existing file' {
         GivenFile 'somefile.zip'
         WhenCreatingArchive 'somefile.zip' -Force
         ThenArchiveCreated 'somefile.zip'
     }
-}
 
-Describe 'New-ZipArchive.when destination exists but it''s a directory' {
-    It 'should fail' {
-        Init
+    It 'rejects when destination is a directory' {
         GivenFile 'dir1.zip/somefile.zip'
         WhenCreatingArchive 'dir1.zip' -Force -ErrorAction SilentlyContinue
         ThenNothingReturned
         ThenError -Matches 'is\ a\ directory'
     }
-}
 
-Describe 'New-ZipArchive.when passing path with valid special characters in path' {
-    It 'should create the file with those characters' {
-        Init
+    It 'supports valid wildcard characters in file name' {
         WhenCreatingArchive 'somefile[1].zip'
         ThenArchiveCreated 'somefile[1].zip'
     }
-}
 
-# Only Windows really has filename character limitations
-if ($IsWindows)
-{
-    Describe 'New-ZipArchive.when passing path with invalid special characters in path' {
+    # Only Windows really has filename character limitations
+    It 'rejects invalid wildcard cahracter in file name' -Skip:(-not $IsWindows) {
         $expectedError = 'Illegal characters in path'
         if ($PSVersionTable.PSVersion.Major -ge 6)
         {
             $expectedError = 'The filename, directory name, or volume label syntax is incorrect.'
         }
-
-        It 'should fail' {
-            Init
-            WhenCreatingArchive 'somefile*.zip' -ErrorAction SilentlyContinue
-            ThenNothingReturned
-            ThenError -Matches $expectedError
-        }
+        WhenCreatingArchive 'somefile*.zip' -ErrorAction SilentlyContinue
+        ThenNothingReturned
+        ThenError -Matches $expectedError
     }
 }
